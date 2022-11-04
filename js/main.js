3fe1d5f28ffb3214ed67165ee43ae4333fb9bbeb
NicholasPeara92 <nicholas.peara@gmail.com>
2022-11-04 15:18:35 +0100

    Aggiunge funzione creazione numeri

--- js/main.js
+++ js/main.js
@@ -5,3 +5,15 @@
 // Dopo 30 secondi i numeri scompaiono e l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
 // Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
 
+// FUNZIONI
+
+//funzione creazione numeri casuali
+function getRndInteger(min, max){
+    return Math.floor(Math.random()* (max - min + 1) + min);
+}
+
+
+
+// ARRAY
+
+const numberArray = [];
